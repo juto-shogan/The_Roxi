616821376dddb25bff5111ff5a1007a9792eb20a
juto-shogan <somtombonu53@gmail.com>
2025-01-18 20:54:47 +0100

    added

--- model_sample.py
+++ model_sample.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 import tensorflow as tf
 
 # Load the model and tokenizer
-model_name = "phi-4"  
+model_name = "microsoft/phi-4"  
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
